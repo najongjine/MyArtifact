0ec9f843597ecb3778a8200f9e3401af940154a0
najongjine <54340913+najongjine@users.noreply.github.com>
2022-08-17 17:38:19 +0900

    .

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 3f;
+
+    Rigidbody2D myBody;
+    Vector2 moveVector;
+    SpriteRenderer sr;
+    Animator anim;
+
+    float harvestTimer;
+    bool isHarvesting;
+    GameObject artifact;
+
+    string MOVEMENT_AXIS_X = "Horizontal";
+    string MOVEMENT_AXIS_Y = "Vertical";
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim=GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlipSprite();
+    }
+    private void FixedUpdate()
+    {
+        if (isHarvesting)
+        {
+            myBody.velocity = Vector2.zero;
+        }
+        else
+        {
+            moveVector = new Vector2(Input.GetAxis(MOVEMENT_AXIS_X), Input.GetAxis(MOVEMENT_AXIS_Y));
+            if (moveVector.sqrMagnitude>1)
+            {
+                moveVector=moveVector.normalized;
+                anim.SetBool("Walk", true);
+            }
+            if (moveVector.sqrMagnitude > 0)
+            {
+                anim.SetBool("Walk", true);
+            }
+            else
+            {
+                anim.SetBool("Walk", false);
+            }
+            myBody.velocity = new Vector2(moveVector.x * movementSpeed, moveVector.y * movementSpeed);
+
+        }
+
+    }
+    void FlipSprite()
+    {
+        if (Input.GetAxisRaw(MOVEMENT_AXIS_X) ==1)
+        {
+            sr.flipX = false;
+        }
+        else if (Input.GetAxisRaw(MOVEMENT_AXIS_X) == -1)
+        {
+            sr.flipX = true;
+        }
+
+    }
+
+}
